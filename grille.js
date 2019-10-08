7a5f9f7bb06372455c95d7883f8c2e22df1cdc87
nmdoir <nmdoir@gmail.com>
2019-10-08 19:07:02 +0200

    Objet case

---
+++ grille.js
@@ -0,0 +1,17 @@
+//Objet case
+function Case(id, etat, arme, joueur, clic) {
+    this.id = id;
+    this.etat = etat;
+    this.arme = arme;
+    this.joueur = joueur;
+    this.clic = clic;
+}
+
+//Générer no access cases
+function noAccessCases() {
+    var nb_cases = 0
+    for (nb_cases; nb_cases < 15; nb_cases++) {
+    return Math.floor(Math.random() * 99);}
+}
+}
+
