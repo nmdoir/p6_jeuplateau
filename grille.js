e4079a4ebc1c2da3391bf9f7f2b5028a6397110b
nmdoir <nmdoir@gmail.com>
2019-10-09 10:30:33 +0200

    fonctions positions

--- grille.js
+++ grille.js
@@ -26,11 +26,26 @@ function noAccessCases() {
 function posJoueurs() {
     var nb_joueurs = 0;
     var pos_joueurs = [];
-    var random_pos = "";
+    var random_pos_joueur = "";
     for (nb_joueurs; nb_joueurs < 2; nb_joueurs++) {
-        random_pos = Math.floor(Math.random() * 99);
-        pos_joueurs.push(random_pos);
+        random_pos_joueur = Math.floor(Math.random() * 99);
+        pos_joueurs.push(random_pos_joueur);
     }
     return pos_joueurs;
+}
 
-    //comment checker que la case est vide ?
+//comment checker que la case est vide, sans joueur, pas no access ?
+
+// Générer positions armes
+    function posArmes() {
+        var nb_armes = 0;
+        var pos_armes = [];
+        var random_pos_arme = "";
+        for (nb_armes; nb_armes < 7; nb_armes++) {
+            random_pos_arme = Math.floor(Math.random() * 99);
+            pos_armes.push(random_pos_arme);
+        }
+        return pos_armes;
+    }
+
+    //Créer une fonction pour math.random au lieu d'utiliser la formule 3 fois ?
