1e796b844f150fde276a426a97d18b08c50b0863
nmdoir <nmdoir@gmail.com>
2019-10-09 09:34:42 +0200

    fonctions math random

--- grille.js
+++ grille.js
@@ -1,4 +1,5 @@
 //Objet case
+
 function Case(id, etat, arme, joueur, clic) {
     this.id = id;
     this.etat = etat;
@@ -8,10 +9,28 @@ function Case(id, etat, arme, joueur, clic) {
 }
 
 //Générer no access cases
+
 function noAccessCases() {
-    var nb_cases = 0
-    for (nb_cases; nb_cases < 15; nb_cases++) {
-    return Math.floor(Math.random() * 99);}
-}
+    var nb_cases = 0;
+    var random_id = "";
+    var no_access_id = [];
+    for (nb_cases; nb_cases < 16; nb_cases++) {
+        random_id = Math.floor(Math.random() * 99);
+        no_access_id.push(random_id);
+    }
+    return no_access_id;
 }
 
+//Générer position joueurs
+
+function posJoueurs() {
+    var nb_joueurs = 0;
+    var pos_joueurs = [];
+    var random_pos = "";
+    for (nb_joueurs; nb_joueurs < 2; nb_joueurs++) {
+        random_pos = Math.floor(Math.random() * 99);
+        pos_joueurs.push(random_pos);
+    }
+    return pos_joueurs;
+
+    //comment checker que la case est vide ?
